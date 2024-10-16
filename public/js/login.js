ceddb978ee1327906b21d5717fb7d72827d9afdb
tleavitt25526 <tleavitt25526@gmail.com>
2024-10-16 11:31:45 -0400

    Found out what bug was - email and pass validation

--- public/js/login.js
+++ public/js/login.js
@@ -4,7 +4,7 @@ const loginFormHandler = async (event) => {
     // Collect values from the login form
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-  
+
     if (email && password) {
       // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
@@ -39,7 +39,7 @@ const loginFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert(response.statusText);
+        alert(`${response.status}`);
       }
     }
   };
